94596d22e4a87a40211dcad1ba8190905eee5297
Zitorious <39710566+Zitorious@users.noreply.github.com>
2024-03-31 22:41:32 -0400

    Update run.sh

--- casaos-add-lan-ip-to-nextcloud-config/run.sh
+++ casaos-add-lan-ip-to-nextcloud-config/run.sh
@@ -4,7 +4,7 @@
 lan_ip=$(hostname -I | awk '{print $1}')
 
 # Backup the original config.php file
-cp /DATA/AppData/big-bear-nextcloud/html/config/config.php mnt/Storage1/DATA/AppData/big-bear-nextcloud/html/config/config.php.bak
+cp mnt/Storage1/DATA/AppData/big-bear-nextcloud/html/config/config.php mnt/Storage1/DATA/AppData/big-bear-nextcloud/html/config/config.php.bak
 
 # Add the LAN IP to the config.php file
 awk -v ip="$lan_ip" '/0 => '\''localhost'\''/{print; print "    1 => '\''"ip"'\'',"; next}1' mnt/Storage1/DATA/AppData/big-bear-nextcloud/html/config/config.php.bak > mnt/Storage1/DATA/AppData/big-bear-nextcloud/html/config/config.php
