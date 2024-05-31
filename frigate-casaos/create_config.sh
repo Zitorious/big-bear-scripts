cc7faae468944bef2df5258ce732c55e69627aec
Zitorious <39710566+Zitorious@users.noreply.github.com>
2024-05-30 22:08:06 -0400

    Update create_config.sh

--- frigate-casaos/create_config.sh
+++ frigate-casaos/create_config.sh
@@ -16,9 +16,23 @@ cameras:
         - path: rtsp://cosmo:cosmotheyorkie@192.168.1.182:8554/unicast
           roles:
             - detect
+            - record
+            - rtmp
     detect:
+      fps: 25
+      enabled: True
       width: 1280
       height: 720
+detectors:
+  coral1:
+    type: edgetpu
+    device: pci:0
+  coral2:
+    type: edgetpu
+    device: pci:1
+objects:
+  track:
+    - person
 EOL
 
 echo "Configuration written to $CONFIG_PATH"
